fe19b12b4d2952d7a9cd25bafd74172dc22f9d72
Erick Daniel Teixeira Vier <erixkat10@gmail.com>
2024-08-22 14:56:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fileInput = document.getElementById('fileInput');
 const fileNameDisplay = document.getElementById('fileName');
-const fileSizeDisplay = document.getElementById('fileSize'); // Adicionado
+const fileSizeDisplay = document.getElementById('fileSize');
 const sendButton = document.getElementById('sendButton');
 const qrCodeContainer = document.querySelector('.qr-code-container');
 const qrCodeImage = document.getElementById('qrCode');
@@ -18,7 +18,7 @@ function displayFileName() {
     if (file) {
         fileNameDisplay.innerHTML = `Arquivo selecionado: <strong>${file.name}</strong>`;
         const fileSize = formatFileSize(file.size);
-        document.getElementById("fileSize").textContent = `Tamanho do arquivo: ${fileSize}`;
+        fileSizeDisplay.textContent = `Tamanho do arquivo: ${fileSize}`;
     }
 }
 
@@ -34,11 +34,58 @@ function formatFileSize(size) {
     }
 }
 
+async function shortenURL(longUrl) {
+    try {
+        const response = await fetch(`https://is.gd/create.php?format=json&url=${encodeURIComponent(longUrl)}`);
+        if (response.ok) {
+            const data = await response.json();
+            return data.shorturl;
+        } else {
+            throw new Error('Erro ao encurtar a URL');
+        }
+    } catch (error) {
+        console.error('Erro ao encurtar a URL:', error);
+        return longUrl; // Retorna o URL original em caso de erro
+    }
+}
+
+async function copyDownloadLink() {
+    const downloadLink = document.getElementById("downloadButton").getAttribute("href");
+    const fileName = document.getElementById("fileName").textContent.replace('Arquivo selecionado: ', '').replace('<strong>', '').replace('</strong>', '');
+
+    // Gerar um link para a página de informações do arquivo
+    const infoPageLink = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
+
+    try {
+        const shortUrl = await shortenURL(infoPageLink);
+        // Copiar o link encurtado para a área de transferência
+        const tempInput = document.createElement("input");
+        tempInput.value = shortUrl;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+
+        // Exibir mensagem de sucesso
+        const copyMessage = document.createElement("p");
+        copyMessage.textContent = "Link copiado com sucesso!";
+        copyMessage.classList.add("copy-message");
+        document.body.appendChild(copyMessage);
+
+        // Remover mensagem após alguns segundos
+        setTimeout(() => {
+            document.body.removeChild(copyMessage);
+        }, 3000);
+    } catch (error) {
+        console.error('Erro ao encurtar a URL:', error);
+    }
+}
+
 function generateQRCode() {
     const file = fileInput.files[0];
     if (file) {
         const fileReader = new FileReader();
-        fileReader.onload = function(event) {
+        fileReader.onload = async function(event) {
             const fileData = event.target.result;
             const qrCodeData = 'data:' + file.type + ';base64,' + fileData.split(',')[1];
 
@@ -50,7 +97,7 @@ function generateQRCode() {
             window.localStorage.setItem('fileSize', fileSize);
 
             // URL da página de informações do arquivo
-            const infoPageUrl = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
+            const infoPageUrl = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
 
             // Criar um link que inclui a URL da página de informações
             const qrCodeLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(infoPageUrl)}`;
@@ -65,38 +112,11 @@ function generateQRCode() {
             copyButton.style.display = 'block';
 
             // Passar os dados do arquivo para a página de informações
-            const infoPageLink = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
+            const infoPageLink = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
             window.localStorage.setItem('infoPageLink', infoPageLink);
         }
         fileReader.readAsDataURL(file);
     }
-}
-
-function copyDownloadLink() {
-    const downloadLink = document.getElementById("downloadButton").getAttribute("href");
-    const fileName = document.getElementById("fileName").textContent;
-
-    // Gerar um link para a página de informações do arquivo
-    const infoPageLink = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
-
-    // Copiar o link para a área de transferência
-    const tempInput = document.createElement("input");
-    tempInput.value = infoPageLink;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-
-    // Exibir mensagem de sucesso
-    const copyMessage = document.createElement("p");
-    copyMessage.textContent = "Link copiado com sucesso!";
-    copyMessage.classList.add("copy-message");
-    document.body.appendChild(copyMessage);
-
-    // Remover mensagem após alguns segundos
-    setTimeout(() => {
-        document.body.removeChild(copyMessage);
-    }, 3000);
 }
 
 function downloadFile() {
