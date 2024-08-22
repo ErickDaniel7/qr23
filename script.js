f279deb4956a73e1866bae86fbe64d5e364906ba
Erick Daniel Teixeira Vier <erixkat10@gmail.com>
2024-08-22 15:51:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ async function copyDownloadLink() {
     const fileName = document.getElementById("fileName").textContent.replace('Arquivo selecionado: ', '').replace('<strong>', '').replace('</strong>', '');
 
     // Gerar um link para a página de informações do arquivo
-    const infoPageLink = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
+    const infoPageLink = `https://qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
 
     try {
         const shortUrl = await shortenURL(infoPageLink);
@@ -81,7 +81,7 @@ async function copyDownloadLink() {
     }
 }
 
-function generateQRCode() {
+async function generateQRCode() {
     const file = fileInput.files[0];
     if (file) {
         const fileReader = new FileReader();
@@ -97,10 +97,11 @@ function generateQRCode() {
             window.localStorage.setItem('fileSize', fileSize);
 
             // URL da página de informações do arquivo
-            const infoPageUrl = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
+            const infoPageUrl = `https://qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
 
             // Criar um link que inclui a URL da página de informações
-            const qrCodeLink = `qr23.vercel.app/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(infoPageUrl)}`;
+            const shortUrl = await shortenURL(infoPageUrl);
+            const qrCodeLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(shortUrl)}`;
 
             qrCodeImage.src = qrCodeLink;
             qrCodeContainer.style.display = 'block';
@@ -112,7 +113,7 @@ function generateQRCode() {
             copyButton.style.display = 'block';
 
             // Passar os dados do arquivo para a página de informações
-            const infoPageLink = `qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
+            const infoPageLink = `https://qr23.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
             window.localStorage.setItem('infoPageLink', infoPageLink);
         }
         fileReader.readAsDataURL(file);
